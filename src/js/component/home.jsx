f4b8e371f76152f8abce3e9826d7b0bbdb694b27
sergiodsg <sergiodasilva.dev@gmail.com>
2022-09-07 23:00:30 +0000

    Código HTML del postcard dentro del componente home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,34 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div class="postcard">
+			<div class="postcard-header">
+				<h1>My Postcard</h1>
+				<img
+					src="https://raw.githubusercontent.com/breatheco-de/exercise-postcard/e63a7916530cc850bd92aa1c2e19191837fb5c80/.learn/assets/4geeks.png"
+					alt="4Geeks logo"
+				/>
+			</div>
+			<div class="postcard-body">
+				<div class="body-left">
+					<p>Look how aresome! This is a postcard that I created using HTML5 and CSS3 during my 4Geeks Academy Course!</p>
+					<p>This is so cool, can't wait to starting doing more stuff</p>
+				</div>
+				<div class="body-right">
+					<label for="name">
+						<input type="text" placeholder="Some Name" />
+					</label>
+					<label for="email">
+						<input type="text" placeholder="Some email" />
+					</label>
+					<label for="comment">
+						<input type="text" placeholder="Some comment" />
+					</label>
+				</div>
+			</div>
+			<div class="postcard-footer">
+				<button>Send My Postcard</button>
+			</div>
 		</div>
 	);
 };
